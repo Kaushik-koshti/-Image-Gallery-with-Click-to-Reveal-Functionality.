3f4d466736077ab2fa7d76240febf8e850c3aff2
Kaushik-koshti <kaushikkoshti1511@gmail.com>
2024-10-15 19:38:31 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const image = document.getElementById('img1');
+
+image.addEventListener('click', function () {
+    document.getElementById("img7").src = "https://i.pinimg.com/736x/d1/28/65/d128655b7bb0d217ce09252436871287.jpg";
+    document.getElementById("img7").style.height = "300px"
+    document.getElementById("img7").style.width = "600px"
+    document.getElementById("img7").style.borderRadius = "10px"
+    document.getElementById("img7").style.overflow = "hidden"
+});
+
+const imge = document.getElementById("img2");
+
+imge.addEventListener('click', function () {
+    document.getElementById("img7").src = "https://i.pinimg.com/736x/bb/52/d7/bb52d7ca602405906fe9ac0223f9c651.jpg";
+    document.getElementById("img7").style.height = "300px"
+    document.getElementById("img7").style.width = "600px"
+    document.getElementById("img7").style.borderRadius = "10px"
+    document.getElementById("img7").style.overflow = "hidden"
+
+});
+
+const imag = document.getElementById("img3");
+
+imag.addEventListener('click', function () {
+    document.getElementById("img7").src = "https://i.pinimg.com/736x/87/a3/41/87a3418da54fbd2f64d28e8429fa55c4.jpg";
+    document.getElementById("img7").style.height = "300px"
+    document.getElementById("img7").style.width = "600px"
+    document.getElementById("img7").style.borderRadius = "10px"
+    document.getElementById("img7").style.overflow = "hidden"
+
+});
+const img = document.getElementById("img4");
+
+img.addEventListener('click', function () {
+    document.getElementById("img7").src = "https://i.pinimg.com/736x/56/42/11/56421193096506d6e8957035cb329f6b.jpg";
+    document.getElementById("img7").style.height = "300px"
+    document.getElementById("img7").style.width = "600px"
+    document.getElementById("img7").style.borderRadius = "10px"
+    document.getElementById("img7").style.overflow = "hidden"
+
+});
+const img5 = document.getElementById("img5");
+
+img5.addEventListener('click', function () {
+    document.getElementById("img7").src = "https://i.pinimg.com/736x/ff/7f/48/ff7f485d787bd8368d284834d5815d80.jpg";
+    document.getElementById("img7").style.height = "300px"
+    document.getElementById("img7").style.width = "600px"
+    document.getElementById("img7").style.borderRadius = "10px"
+    document.getElementById("img7").style.overflow = "hidden"
+
+});
+const img6 = document.getElementById("img6");
+
+img6.addEventListener('click', function () {
+    document.getElementById("img7").src = "https://i.pinimg.com/736x/de/61/9d/de619d944cc6c1ddb49f6cb4a52c8464.jpg";
+    document.getElementById("img7").style.height = "300px"
+    document.getElementById("img7").style.width = "600px"
+    document.getElementById("img7").style.borderRadius = "10px"
+    document.getElementById("img7").style.overflow = "hidden"
+
+});
